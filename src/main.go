feab773b5e3d04b77bd9e3c7db69fcdebd1e8040
Fawn <rubiefawn@gmail.com>
2025-02-19 21:04:45 -0700

    scramble a bunch of stuff

---
+++ src/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	. "fmt"
+	"html/template"
+	"mime"
+	"net/http"
+	"os"
+	fpath "path/filepath"
+	"sort"
+	"time"
+	"github.com/BurntSushi/toml"
+)
+
+// Top-level just lists all songs and allows the latest version of each
+// to be played from there
+
+// Clicking on a song navigates to that song's specific page, where
+// different versions can be selected and played
+
+// Pages specific to a version of a song also exist for sharing
+// purposes, but aren't a main feature otherwise
+
+// TODO: Allow user-defined network port via CLI flags
+// TODO: Allow user-defined audio directory via CLI flags
+// TODO: Support for album art
+// TODO: Revision comments
+
+type Revision struct {
+	Path     template.URL
+	Modified time.Time
+	// Comments string
+}
+
+type SongInfo struct {
+	Name              string
+	NameIsPlaceholder bool `toml:"name_is_placeholder"`
+	Emoji             string
+	IsReleased        bool `toml:"released"`
+}
+
+type Song struct {
+	SongInfo	
+	Revisions         []Revision
+}
+
+var songs []Song
+var audio_dir string = "audio"
+
+func main() {
+	mime.AddExtensionType(".css", "text/css")
+	mime.AddExtensionType(".js", "text/javascript")
+	mime.AddExtensionType(".mjs", "text/javascript")
+
+	mime.AddExtensionType(".wav", "audio/wav")
+	mime.AddExtensionType(".mp3", "audio/mpeg")
+	mime.AddExtensionType(".flac", "audio/flac")
+	mime.AddExtensionType(".aac", "audio/aac")
+
+	// TODO: Parse CLI flags:
+	// - user-defined audio directory via -a | --audio-dir
+	// - user-defined port via -p | --port
+
+	audio_dir = "//temperance/media/sketches" // WARN: remove this
+	var err error
+	if songs, err = scan_all_songs(audio_dir); err != nil {
+		Printf("error: %s\n", err.Error())
+	}
+
+	for _, song := range songs {
+		Printf("%s %s %v %v\n", song.Emoji, song.Name, song.IsReleased, song.NameIsPlaceholder)
+		for _, rev := range song.Revisions {
+			Printf("   %s\n", string(rev.Path))
+		}
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("GET /audio/", http.StripPrefix("/audio/", http.FileServer(http.Dir(audio_dir))))
+	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	mux.HandleFunc("GET /song/{song}", serve_song)
+	mux.HandleFunc("GET /", serve_all_songs)
+	http.ListenAndServe(":8080", mux)
+}
+
+func scan_all_songs(in_path string) (songs []Song, err error) {
+	in_path = fpath.Clean(in_path)
+	toml_path := fpath.ToSlash(fpath.Join(in_path, "pepper.toml"))
+	var data []byte
+	if data, err = os.ReadFile(toml_path); err != nil {
+		return
+	}
+	var pepper_toml struct {Songs []SongInfo}
+	if err = toml.Unmarshal(data, &pepper_toml); err != nil {
+		return
+	}
+
+	for _, song_info := range pepper_toml.Songs{
+		if (song_info.Emoji == "") {
+			song_info.Emoji = "🌶️"
+		}
+		song_path := fpath.ToSlash(fpath.Join(in_path, song_info.Name))
+		var r []Revision
+		if r, err = scan_revisions(song_path); err != nil {
+			return
+		}
+		songs = append(songs, Song{song_info, r})
+	}
+	sort.Slice(songs, func(a int, b int) bool { return songs[a].Revisions[0].Modified.After(songs[b].Revisions[0].Modified) })
+	return
+}
+
+func scan_revisions(song_path string) (revisions []Revision, err error) {
+	var files []os.DirEntry
+	if files, err = os.ReadDir(song_path); err != nil {
+		return
+	}
+	for _, rev := range files {
+		var i os.FileInfo
+		if i, err = rev.Info(); err != nil {
+			return
+		}
+		ext := fpath.Ext(rev.Name())
+		if !(ext == ".mp3" || ext == ".wav" || ext == ".flac" || ext == ".aac") {
+			continue
+		}
+		revisions = append(revisions, Revision{template.URL(rev.Name()), i.ModTime()})
+	}
+	sort.Slice(revisions, func(a int, b int) bool { return revisions[a].Modified.After(revisions[b].Modified) })
+	return
+}
+
+func serve_song(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseGlob("template/*.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	if err = tmpl.ExecuteTemplate(w, "song", songs); err != nil {
+		Println(err)
+	}
+}
+
+func serve_all_songs(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseGlob("template/*.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	if err = tmpl.ExecuteTemplate(w, "main", songs); err != nil {
+		Println(err)
+	}
+}
---
+++ static/js/common.mjs
@@ -0,0 +1,61 @@
+const audio = document.querySelector("audio");
+const progress = document.getElementById("playback-progress");
+const btn_theme = document.getElementById("btn-theme-toggle");
+const time = document.getElementById("playback-time");
+const btn_main = document.getElementById("btn-main");
+const name_main = document.getElementById("name-main");
+
+function set_song(name, url) {
+	audio.src = url;
+	name_main.innerHTML = name;
+}
+
+function toggle_playback(force_play) {
+	if (!audio.src) { return; }
+	if (force_play ?? audio.paused) {
+		audio.play();
+	} else {
+		audio.pause();
+	}
+}
+
+function audio_progress_readable() {
+	const bm = `${Math.floor(audio.currentTime / 60)}`.padStart(2, '0');
+	const bs = `${Math.floor(audio.currentTime % 60)}`.padStart(2, '0');
+	const em = `${Math.floor(audio.duration / 60)}`.padStart(2, '0');
+	const es = `${Math.floor(audio.duration % 60)}`.padStart(2, '0');
+	return `${bm}:${bs}/${em}:${es}`;
+}
+
+audio.addEventListener("timeupdate", _ => {
+	progress.value = audio.currentTime;
+	progress.max = audio.duration;
+	time.innerHTML = audio_progress_readable();
+});
+
+btn_theme.addEventListener("click", _ => {
+	const root_html = document.querySelector("html");
+	const theme = root_html.getAttribute("theme");
+	root_html.setAttribute("theme", theme == "dark" ? "light" : "dark");
+});
+btn_main.addEventListener("click", _ => toggle_playback());
+progress.addEventListener("input", _ => audio.fastSeek(progress.value));
+document.querySelectorAll(".song").forEach(song => {
+	const version_selector = song.querySelector(".song-revision");
+	const name = song.querySelector(".song-name").innerHTML;
+	const btn = song.querySelector(".btn-play-song");
+	const btn_check = btn.querySelector("input[type=checkbox]");
+	btn.addEventListener("click", e => {
+		// Pause if this revision is already playing
+		const already_playing_this_rev = version_selector.value == audio.getAttribute("src");
+		if (!already_playing_this_rev) {
+			set_song(`${name} @ ${version_selector.selectedOptions[0].innerHTML}`, version_selector.value);
+			document.querySelectorAll("button>input[type=checkbox]").forEach(it => it.checked = false);
+			btn_check.checked = true;
+			toggle_playback(true);
+		} else {
+			toggle_playback();
+			btn_check.checked = !audio.paused;
+		}
+	});
+});
